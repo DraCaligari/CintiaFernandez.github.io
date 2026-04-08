90d53970616c17b8e1c5cdeefd9c1c8593e805e8
nicolle.gomez <nicolle.gomez@nuevoleon.gob.mx>
2026-04-08 16:02:44 -0600

    feat: add Vercel adapter and update dependencies for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://DraCaligari.github.io',
-  base: '/CintiaFernandez.github.io',
-  output: 'static',
+  output: 'server',
+  adapter: vercel(),
 });
